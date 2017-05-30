5377de55e52a5faae5f2ff134bef50e66b51bc54
Matt McConway <mattmcconway@Matts-MacBook-Pro.local>
2017-05-30 18:03:21 +1200

    Chapter 4 Working
    Just the first 5 done so far

---
+++ Chapter 4 - Working with Lists/ex4-2_pp60_Animals.py
@@ -0,0 +1,28 @@
+"""
+4-2. Animals: Think of at least three different animals that have a common
+characteristic. Store the names of these animals in a list, and then use a for
+loop to print out the name of each animal.
+    • Modify your program to print a statement about each animal, such as
+      A dog would make a great pet.
+
+    • Add a line at the end of your program stating what these animals have in
+      common  You could print a sentence such as Any of these animals would
+      make a great pet!
+"""
+
+commonAnimals = ["Dog", "Cat", "Crocodile"]
+
+for animal in commonAnimals:
+    print(animal)
+
+print("\n")
+# Statement about each animal
+
+for animal in commonAnimals:
+    print("A " + animal + "has four legs.")
+print("\n")
+
+# Final statement out side of loop
+for animal in commonAnimals:
+    print("A " + animal + "has four legs.")
+print("All of these animals have four legs!")
